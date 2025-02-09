b78be7a0e3f6b66ecb6707b3cb147542136aa596
Ghulam Qadir <hafizghulamqadir.2005@gmail.com>
2025-02-09 19:53:49 +0500

    view product component added

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,67 +1,8 @@
-"use client";
-// import ProductDetailsSkeleton from "@/components/ProductDetailsSkeleton";
-// import { client } from "@/sanity/lib/client";
-// import Image from "next/image";
-// import { useEffect, useState } from "react";
-
-// interface Props {
-//   params: Promise<{
-//     slug: string;
-//   }>;
-// }
-// interface TProduct {
-//   name: string;
-//   price: number;
-//   image: string;
-//   description: string;
-//   stockLevel: number;
-// }
+import ViewProduct from "@/components/ViewProduct";
 
 async function ProductPage(props: { params: Promise<{ slug: string }> }) {
-  // const [data, setProdData] = useState<TProduct[] | null>(null);
   const params = await props.params;
   const { slug } = params;
-  console.log("slug=>>", slug);
-  // useEffect(() => {
-  //   const fetchProd = async () => {
-  //     const prodData = await client.fetch(
-  //       `*[_type=="product" && slug.current=="${slug}"]
-  //       { name, "image":image.asset->url, price, description, stockLevel}`
-  //     );
-  //     setProdData(prodData);
-  //   };
-  //   fetchProd();
-  // }, []);
-  // if (!data) {
-  //   return <ProductDetailsSkeleton />;
-  // }
-  // const { name, image, price, description, stockLevel } = data[0];
-  return (
-    <div className="my-8 flex flex-wrap gap-x-4 ">
-      <div>
-        {/* <Image
-          src={image}
-          height={550}
-          width={500}
-          alt={name}
-          priority
-          className="lg:w-[450px] lg:h-[500px] md:h-[400px] md:w-[350px] h-[250px] w-[250px]"
-        />
-      </div>
-      <div className="px-1 md:mt-6 mt-3">
-        <p className="text-xl md:text-2xl lg:text-3xl font-bold">{name}</p>
-        <p className="my-2 font-bold text-gray-500">
-          $<span className="text-lg md:text-xl text-black">{price}.00</span>
-        </p>
-        <p className="text-sm my-2">{stockLevel} items left in stock</p>
-        {description && <p className="text-lg">{description}</p>}
-        <div className="flex justify-center w-full mt-7">
-          <button className="bg-[#FB2E86] h-[30px] px-[10px] text-white">
-            Add to Cart
-          </button>
-        </div> */}
-      </div>
-    </div>
-  );
+  return <ViewProduct slug={slug} />;
 }
 export default ProductPage;
